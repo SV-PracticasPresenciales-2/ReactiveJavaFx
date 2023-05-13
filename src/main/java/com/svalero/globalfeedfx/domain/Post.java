20a432723d2bda4824123f16ebfde565cf6e6280
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2023-05-13 11:47:06 +0200

    Domains created

---
+++ src/main/java/com/svalero/globalfeedfx/domain/Post.java
@@ -0,0 +1,9 @@
+package com.svalero.globalfeedfx.domain;
+
+public class Post {
+    public int id;
+    public String message;
+    public String postDate;
+    public int likes;
+    public User userPost;
+}
---
+++ src/main/java/com/svalero/globalfeedfx/domain/User.java
@@ -0,0 +1,11 @@
+package com.svalero.globalfeedfx.domain;
+
+public class User {
+    public int id;
+    public String username;
+    public String password;
+    public String name;
+    public String biography;
+    public String registerDate;
+    public int phoneNumber;
+}
