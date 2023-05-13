89632ecadab98b25e2a98ca7b3f28c976fa499dd
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2023-05-13 12:08:49 +0200

    GlobalFeed API added

--- src/main/java/com/svalero/globalfeedfx/domain/Post.java
+++ src/main/java/com/svalero/globalfeedfx/domain/Post.java
@@ -1,5 +1,12 @@
 package com.svalero.globalfeedfx.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
     public int id;
     public String message;
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.svalero.globalfeedfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.svalero.globalfeedfx to javafx.fxml;
-    exports com.svalero.globalfeedfx;
-}
